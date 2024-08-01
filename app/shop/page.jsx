3b2c1312b906cdbae78dccdc7dfd7fb5b69ad8f2
aislusha96 <arslanova1996ais@list.ru>
2024-08-01 14:03:55 +0300

    new

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -2,7 +2,7 @@ import Menu from "../menu";
 
 const Shop = () => {
     return (
-        <div className="relative flex flex-col h-screen bg-black text-white overflow-hidden">
+        <div className="relative flex flex-col h-screen bg-black text-white overflow-hidden speech">
             <div className="absolute top-0 left-0 w-full h-[89.7%] bg-white opacity-5 z-20 rounded-[50px]"></div>
             <Menu className="fixed bottom-0 w-full z-30"/>
         </div>
