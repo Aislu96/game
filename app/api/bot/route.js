65e3b00e0d21ee019de0a81aa5f20f57ce165b72
ishaku <ishakumarch@gmail.com>
2024-08-05 17:46:29 +0300

    route fix

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import TelegramBot from "node-telegram-bot-api";
 import { supabase } from "../../utils/supabase/server";
 
-export const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
   polling: false,
 });
 
