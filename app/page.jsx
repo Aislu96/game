fcdb70838739840bda6bf72e42a936752dacd775
ishaku <ishakumarch@gmail.com>
2024-08-09 15:34:46 +0300

    wheel ish

--- app/page.jsx
+++ app/page.jsx
@@ -265,7 +265,7 @@ const Page = () => {
 
   return (
     <div className="relative flex flex-col h-screen bg-black text-white ">
-      <div className="absolute top-0 right-0 w-[600px] h-[600px] rounded-full bg-orange-500 opacity-30 blur-[120px] -translate-y-1/4 translate-x-1/4"></div>
+      {/* <div className="absolute top-0 right-0 w-[600px] h-[600px] rounded-full bg-orange-500 opacity-30 blur-[120px] -translate-y-1/4 translate-x-1/4"></div>
       <div className="absolute bottom-0 left-0 w-[600px] h-[600px] rounded-full bg-purple-600 opacity-30 blur-[120px] translate-y-1/4 -translate-x-1/4"></div>
       <div className="absolute top-1/2 left-1/2 w-[400px] h-[400px] rounded-full bg-blue-500 opacity-20 blur-[100px] -translate-x-1/2 -translate-y-1/2"></div>
       <div className="absolute top-0 left-0 w-full  h-[90vh] z-10">
@@ -273,7 +273,7 @@ const Page = () => {
         <div
           className={`w-full opacity-5 absolute z-10 bottom-0 h-0 triangle`}
         ></div>
-      </div>
+      </div> */}
 
       <Game />
 
