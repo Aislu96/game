bea0459c5e8f067acc2f5460c20148f8664a91a6
ishaku <ishakumarch@gmail.com>
2024-08-09 15:42:31 +0300

    wheel thing

--- app/game/game.jsx
+++ app/game/game.jsx
@@ -5,7 +5,7 @@ import LabeledIcon from "./labeledIcon";
 
 const Game = () => {
   return (
-    <div className="flex-grow  flex-col justify-center items-center space-y-4  !z-50 ">
+    <div className="flex-grow  flex-col justify-center items-center space-y-4  z-[100] ">
       {/* <LabeledIcon /> */}
 
       <ScoreDisplay />
